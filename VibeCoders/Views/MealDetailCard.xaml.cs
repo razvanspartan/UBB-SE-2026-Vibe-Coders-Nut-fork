0dc69b2cb0cf1114ab732a110f2beb9ed7eb323f
Alawerga <adrianfdez.rdez@gmail.com>
2026-03-30 16:23:04 +0200

    Meal detail card ui implemented. Issue #123 (#219)

---
+++ VibeCoders/Views/MealDetailCard.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace VibeCoders.Views
+{
+    public partial class MealDetailCard : UserControl
+    {
+        public MealDetailCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
