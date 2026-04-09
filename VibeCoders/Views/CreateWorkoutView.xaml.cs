d7fb1a3ca40fb6d4ad5df0f417572f650d8fde68
mirko2852 <mirko2852@icloud.com>
2026-04-09 13:27:43 +0200

    Fixed bug related to the remove button inside the list of the custom workouts

--- VibeCoders/Views/CreateWorkoutView.xaml.cs
+++ VibeCoders/Views/CreateWorkoutView.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml.Controls;
+using VibeCoders.Models;
 using VibeCoders.ViewModels;
 
 namespace VibeCoders.Views
@@ -12,6 +13,14 @@ namespace VibeCoders.Views
             ViewModel = App.GetService<CreateWorkoutViewModel>();
             DataContext = ViewModel;
             InitializeComponent();
+        }
+
+        private void RemoveExercise_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is TemplateExercise exercise)
+            {
+                ViewModel.Exercises.Remove(exercise);
+            }
         }
     }
 }
