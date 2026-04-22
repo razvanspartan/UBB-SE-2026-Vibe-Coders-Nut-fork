173a79ceb59faaec5fa9416efb16c1e5ec5880ab
Razvan Bichiu <razvan.bichiu@msg.group>
2026-04-23 00:36:52 +0300

    modify

--- VibeCoders.Tests/Unit/ViewModels/ActiveWorkoutViewModelTests.cs
+++ VibeCoders.Tests/Unit/ViewModels/ActiveWorkoutViewModelTests.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using FluentAssertions;
 using NSubstitute;
 using VibeCoders.Models;
@@ -12,185 +10,190 @@ namespace VibeCoders.Tests.Unit.ViewModels
 {
     public class ActiveWorkoutViewModelTests
     {
+        private const int DefaultClientId = 1;
+        private const int DefaultTemplateId = 1;
+        private const int DefaultNotificationId = 1;
+        private const int SingleTargetSet = 1;
+        private const int MultipleTargetSets = 2;
+        private const int GoalTargetSets = 3;
+        private const int CompletedSetReps = 10;
+        private const double CompletedSetWeight = 100;
+        private const double CurrentSetRepsInput = 12;
+        private const double CurrentSetWeightInput = 80;
+
         private readonly IClientService clientServiceMock;
         private readonly INavigationService navigationMock;
-        private readonly WorkoutUiState uiState;
-        private readonly ActiveWorkoutViewModel systemUnderTest;
+        private readonly WorkoutUiState workoutState;
+        private readonly ActiveWorkoutViewModel activeWorkoutViewModel;
 
         public ActiveWorkoutViewModelTests()
         {
             this.clientServiceMock = Substitute.For<IClientService>();
             this.navigationMock = Substitute.For<INavigationService>();
-            this.uiState = new WorkoutUiState();
+            this.workoutState = new WorkoutUiState();
             
-            this.systemUnderTest = new ActiveWorkoutViewModel(
+            this.activeWorkoutViewModel = new ActiveWorkoutViewModel(
                 this.clientServiceMock,
-                this.storageMock,
                 this.navigationMock,
-                this.uiState);
+                this.workoutState);
+        }
+
+        private void StartWorkout(int clientId, int templateId, string workoutName, string exerciseName, int targetSets = SingleTargetSet)
+        {
+            this.activeWorkoutViewModel.SelectedGoal = workoutName;
+
+            var workoutTemplate = new WorkoutTemplate { Id = templateId, Name = workoutName, Type = WorkoutType.CUSTOM };
+            workoutTemplate.AddExercise(new TemplateExercise { Name = exerciseName, TargetSets = targetSets });
+
+            this.clientServiceMock.GetAvailableWorkoutsForClient(clientId).Returns(new List<WorkoutTemplate> { workoutTemplate });
+
+            this.activeWorkoutViewModel.ApplyTargetGoalsCommand.Execute(clientId);
         }
 
         [Theory]
         [InlineData(null)]
         [InlineData("")]
         [InlineData("abc")]
-        public void SetRestTime_WhenInvalid_DoesNotStartTimer(string input)
+        public void SetRestTime_WhenInvalid_DoesNotStartTimer(string? restTimeInput)
         {
-            this.systemUnderTest.SetRestTimeCommand.Execute(input);
-            this.systemUnderTest.RestTimeRemaining.Should().Be(0);
+            this.activeWorkoutViewModel.SetRestTimeCommand.Execute(restTimeInput);
+            this.activeWorkoutViewModel.RestTimeRemaining.Should().Be(0);
         }
 
         [Fact]
         public void StartRestTimer_WhenZeroOrNegative_StopsResting()
         {
-            this.systemUnderTest.IsResting = true;
-            this.systemUnderTest.StartRestTimer(0);
-            this.systemUnderTest.IsResting.Should().BeFalse();
-            this.systemUnderTest.RestTimeRemaining.Should().Be(0);
+            this.activeWorkoutViewModel.IsResting = true;
+            this.activeWorkoutViewModel.StartRestTimer(0);
+            this.activeWorkoutViewModel.IsResting.Should().BeFalse();
+            this.activeWorkoutViewModel.RestTimeRemaining.Should().Be(0);
         }
 
         [Fact]
         public void ApplyTargetGoals_WhenValid_LoadsTemplateAndStartsWorkout()
         {
-            this.systemUnderTest.SelectedGoal = "Push";
-            var template = new WorkoutTemplate { Id = 1, Name = "Push", Type = WorkoutType.CUSTOM };
-            template.AddExercise(new TemplateExercise { Name = "Bench", TargetSets = 3 });
-            this.storageMock.GetAvailableWorkouts(1).Returns(new List<WorkoutTemplate> { template });
+            this.activeWorkoutViewModel.SelectedGoal = "Push";
+            var workoutTemplate = new WorkoutTemplate { Id = DefaultTemplateId, Name = "Push", Type = WorkoutType.CUSTOM };
+            workoutTemplate.AddExercise(new TemplateExercise { Name = "Bench", TargetSets = GoalTargetSets });
+            this.clientServiceMock.GetAvailableWorkoutsForClient(DefaultClientId).Returns(new List<WorkoutTemplate> { workoutTemplate });
 
-            this.systemUnderTest.ApplyTargetGoalsCommand.Execute(1);
+            this.activeWorkoutViewModel.ApplyTargetGoalsCommand.Execute(DefaultClientId);
 
-            this.systemUnderTest.IsWorkoutStarted.Should().BeTrue();
-            this.systemUnderTest.WorkoutSessionTitle.Should().Be("Push");
-            this.systemUnderTest.ExerciseRows.Should().HaveCount(1);
-            this.systemUnderTest.CurrentExerciseName.Should().Be("Bench");
+            this.activeWorkoutViewModel.IsWorkoutStarted.Should().BeTrue();
+            this.activeWorkoutViewModel.WorkoutSessionTitle.Should().Be("Push");
+            this.activeWorkoutViewModel.ExerciseRows.Should().HaveCount(1);
+            this.activeWorkoutViewModel.CurrentExerciseName.Should().Be("Bench");
         }
 
         [Fact]
         public void SaveSet_WhenValid_SavesAndAdvances()
         {
-            this.systemUnderTest.IsWorkoutStarted = true;
-            var set = new ActiveSetViewModel { ExerciseName = "Squat", SetIndex = 1, ActualReps = 10, ActualWeight = 100 };
+            this.StartWorkout(DefaultClientId, DefaultTemplateId, "Squat", "Squat", MultipleTargetSets);
+            var activeSet = this.activeWorkoutViewModel.ExerciseRows[0].Sets[0];
+            activeSet.ActualReps = CompletedSetReps;
+            activeSet.ActualWeight = CompletedSetWeight;
             this.clientServiceMock.SaveSet(Arg.Any<WorkoutLog>(), "Squat", Arg.Any<LoggedSet>()).Returns(true);
-            
-            this.systemUnderTest.ExerciseRows.Add(new ActiveExerciseViewModel(new TemplateExercise { Name = "Squat", TargetSets = 2 }, _ => { }));
-            
-            this.systemUnderTest.SaveSetCommand.Execute(set);
 
-            set.IsCompleted.Should().BeTrue();
-            this.systemUnderTest.ErrorMessage.Should().BeEmpty();
+            this.activeWorkoutViewModel.SaveSetCommand.Execute(activeSet);
+
+            activeSet.IsCompleted.Should().BeTrue();
+            this.activeWorkoutViewModel.ErrorMessage.Should().BeEmpty();
         }
 
         [Fact]
         public void SaveSet_WhenServiceFails_SetsErrorMessage()
         {
-            this.systemUnderTest.IsWorkoutStarted = true;
-            var set = new ActiveSetViewModel { ExerciseName = "Squat", SetIndex = 1, ActualReps = 10, ActualWeight = 100 };
+            this.StartWorkout(DefaultClientId, DefaultTemplateId, "Squat", "Squat", MultipleTargetSets);
+            var activeSet = this.activeWorkoutViewModel.ExerciseRows[0].Sets[0];
+            activeSet.ActualReps = CompletedSetReps;
+            activeSet.ActualWeight = CompletedSetWeight;
             this.clientServiceMock.SaveSet(Arg.Any<WorkoutLog>(), "Squat", Arg.Any<LoggedSet>()).Returns(false);
 
-            this.systemUnderTest.SaveSetCommand.Execute(set);
+            this.activeWorkoutViewModel.SaveSetCommand.Execute(activeSet);
 
-            set.IsCompleted.Should().BeFalse();
-            this.systemUnderTest.ErrorMessage.Should().Be("Failed to save set. Please try again.");
+            activeSet.IsCompleted.Should().BeFalse();
+            this.activeWorkoutViewModel.ErrorMessage.Should().Be("Failed to save set. Please try again.");
         }
 
         [Fact]
         public void FinishWorkout_WhenValid_FinalizesAndNavigates()
         {
-            this.systemUnderTest.IsWorkoutStarted = true;
+            this.StartWorkout(DefaultClientId, DefaultTemplateId, "Push", "Bench");
             this.clientServiceMock.FinalizeWorkout(Arg.Any<WorkoutLog>()).Returns(true);
 
-            this.systemUnderTest.FinishWorkoutCommand.Execute(1);
+            this.activeWorkoutViewModel.FinishWorkoutCommand.Execute(DefaultClientId);
 
-            this.systemUnderTest.IsWorkoutStarted.Should().BeFalse();
+            this.activeWorkoutViewModel.IsWorkoutStarted.Should().BeFalse();
             this.navigationMock.Received(1).NavigateToClientDashboard(true);
-        }
-
-        [Fact]
-        public void RepeatWorkout_WhenHasLastLog_SelectsTemplate()
-        {
-            this.systemUnderTest.IsWorkoutStarted = true;
-            this.clientServiceMock.FinalizeWorkout(Arg.Any<WorkoutLog>()).Returns(true);
-            this.systemUnderTest.FinishWorkoutCommand.Execute(1);
-
-            var template = new WorkoutTemplate { Id = 0, Name = "Previous" };
-            this.storageMock.GetAvailableWorkouts(1).Returns(new List<WorkoutTemplate> { template });
-
-            this.systemUnderTest.RepeatWorkoutCommand.Execute(1);
-
-            this.systemUnderTest.SelectedTemplate.Should().Be(template);
         }
 
         [Fact]
         public void LoadNotifications_PopulatesList()
         {
-            var notification = new Notification { Id = 1 };
-            this.clientServiceMock.GetNotifications(1).Returns(new List<Notification> { notification });
+            var notification = new Notification { Id = DefaultNotificationId };
+            this.clientServiceMock.GetNotifications(DefaultClientId).Returns(new List<Notification> { notification });
 
-            this.systemUnderTest.LoadNotificationsCommand.Execute(1);
+            this.activeWorkoutViewModel.LoadNotificationsCommand.Execute(DefaultClientId);
 
-            this.systemUnderTest.Notifications.Should().ContainSingle().Which.Should().Be(notification);
+            this.activeWorkoutViewModel.Notifications.Should().ContainSingle().Which.Should().Be(notification);
         }
 
         [Fact]
         public void ConfirmDeload_RemovesNotification()
         {
-            var notification = new Notification { Id = 1 };
-            this.systemUnderTest.Notifications.Add(notification);
+            var notification = new Notification { Id = DefaultNotificationId };
+            this.activeWorkoutViewModel.Notifications.Add(notification);
 
-            this.systemUnderTest.ConfirmDeloadCommand.Execute(notification);
+            this.activeWorkoutViewModel.ConfirmDeloadCommand.Execute(notification);
 
             this.clientServiceMock.Received(1).ConfirmDeload(notification);
-            this.systemUnderTest.Notifications.Should().BeEmpty();
+            this.activeWorkoutViewModel.Notifications.Should().BeEmpty();
         }
 
         [Fact]
         public void CompleteCurrentSet_UpdatesPendingSet()
         {
-            this.systemUnderTest.SelectedGoal = "Push";
-            var template = new WorkoutTemplate { Id = 1, Name = "Push" };
-            template.AddExercise(new TemplateExercise { Name = "Bench", TargetSets = 1 });
-            this.storageMock.GetAvailableWorkouts(1).Returns(new List<WorkoutTemplate> { template });
-            this.systemUnderTest.ApplyTargetGoalsCommand.Execute(1);
+            this.StartWorkout(DefaultClientId, DefaultTemplateId, "Push", "Bench");
 
-            this.systemUnderTest.CurrentSetRepsInput = 12;
-            this.systemUnderTest.CurrentSetWeightInput = 80;
+            this.activeWorkoutViewModel.CurrentSetRepsInput = CurrentSetRepsInput;
+            this.activeWorkoutViewModel.CurrentSetWeightInput = CurrentSetWeightInput;
 
-            this.systemUnderTest.CompleteCurrentSetCommand.Execute(null);
+            this.activeWorkoutViewModel.CompleteCurrentSetCommand.Execute(null);
 
-            var set = this.systemUnderTest.ExerciseRows[0].Sets[0];
-            set.ActualRepsValue.Should().Be(12);
-            set.ActualWeightValue.Should().Be(80);
+            var activeSet = this.activeWorkoutViewModel.ExerciseRows[0].Sets[0];
+            activeSet.ActualRepsValue.Should().Be(CurrentSetRepsInput);
+            activeSet.ActualWeightValue.Should().Be(CurrentSetWeightInput);
         }
 
         [Fact]
         public void ActiveSetViewModel_Properties_FunctionCorrectly()
         {
             var autoSaveTriggered = false;
-            var set = new ActiveSetViewModel
+            var activeSet = new ActiveSetViewModel
             {
                 AutoSaveHandler = _ => autoSaveTriggered = true
             };
 
-            set.ActualRepsValue = 10;
-            set.ActualWeightValue = double.NaN;
+            activeSet.ActualRepsValue = 10;
+            activeSet.ActualWeightValue = double.NaN;
 
-            set.ActualReps.Should().Be(10);
+            activeSet.ActualReps.Should().Be(10);
             autoSaveTriggered.Should().BeFalse();
 
-            set.ActualWeightValue = 50.5;
-            set.ActualWeight.Should().Be(50.5);
+            activeSet.ActualWeightValue = 50.5;
+            activeSet.ActualWeight.Should().Be(50.5);
             autoSaveTriggered.Should().BeTrue();
         }
 
         [Fact]
-        public void ActiveSetViewModel_WhenNaN_SetsNull()
+        public void ActiveSetViewModel_WhenValueIsNotANumber_SetsNull()
         {
-            var set = new ActiveSetViewModel();
-            set.ActualRepsValue = double.NaN;
-            set.ActualWeightValue = double.NaN;
+            var activeSet = new ActiveSetViewModel();
+            activeSet.ActualRepsValue = double.NaN;
+            activeSet.ActualWeightValue = double.NaN;
 
-            set.ActualReps.Should().BeNull();
-            set.ActualWeight.Should().BeNull();
+            activeSet.ActualReps.Should().BeNull();
+            activeSet.ActualWeight.Should().BeNull();
         }
     }
 }
