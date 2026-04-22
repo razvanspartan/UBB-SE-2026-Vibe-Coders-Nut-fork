cd4a0a81e4f87015564672f040466656b3053223
anisiaaa <bantuanisia@gmail.com>
2026-04-22 18:08:13 +0300

    .

--- VibeCoders.Tests/Unit/ViewModels/AchievementsViewModelTests.cs
+++ VibeCoders.Tests/Unit/ViewModels/AchievementsViewModelTests.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using NSubstitute;
 using VibeCoders.Models;
-using VibeCoders.Repositories.Interfaces;
+using VibeCoders.Services.Interfaces;
 using VibeCoders.ViewModels;
 using Xunit;
 
@@ -12,84 +9,94 @@ namespace VibeCoders.Tests.Unit.ViewModels
 {
     public class AchievementsViewModelTests
     {
-        private readonly IRepositoryAchievements _achievementsRepository;
-        private readonly AchievementsViewModel _sut;
+        private readonly IClientService _clientService;
+        private readonly AchievementsViewModel _achievementsViewModel;
+
+        private const int ValidClientId = 1;
+        private const int ExistingAchievementId = 99;
+        private const int FirstAchievementId = 1;
+        private const int SecondAchievementId = 2;
+        private const string FirstAchievementName = "First Workout";
+        private const string FirstAchievementDescription = "Complete your first workout";
+        private const string FirstAchievementCriteria = "1 workout";
+        private const string FirstAchievementIcon = "&#xE73E;";
+        private const string SecondAchievementName = "10 Workouts";
+        private const string SecondAchievementDescription = "Complete 10 workouts";
+        private const string SecondAchievementCriteria = "10 workouts";
+        private const string SecondAchievementIcon = "&#xE72E;";
+        private const string DatabaseErrorMessage = "Database error";
+
+        private readonly List<Achievement> _mockAchievements;
 
         public AchievementsViewModelTests()
         {
-            _achievementsRepository = Substitute.For<IRepositoryAchievements>();
-            _sut = new AchievementsViewModel(_achievementsRepository);
+            _clientService = Substitute.For<IClientService>();
+            _achievementsViewModel = new AchievementsViewModel(_clientService);
+
+            _mockAchievements = new List<Achievement>
+            {
+                new Achievement
+                {
+                    AchievementId = FirstAchievementId,
+                    Name = FirstAchievementName,
+                    Description = FirstAchievementDescription,
+                    Criteria = FirstAchievementCriteria,
+                    IsUnlocked = true,
+                    Icon = FirstAchievementIcon
+                },
+                new Achievement
+                {
+                    AchievementId = SecondAchievementId,
+                    Name = SecondAchievementName,
+                    Description = SecondAchievementDescription,
+                    Criteria = SecondAchievementCriteria,
+                    IsUnlocked = false,
+                    Icon = SecondAchievementIcon
+                }
+            };
         }
 
         [Fact]
         public void LoadAchievementsCommand_ValidClientId_PopulatesAchievementsAndUpdatesLoadingState()
         {
-            // Arrange
-            var clientId = 1;
-            var mockShowcaseItems = new List<AchievementShowcaseItem>
-            {
-                new AchievementShowcaseItem
-                {
-                    AchievementId = 1,
-                    Title = "First Workout",
-                    Description = "Complete your first workout",
-                    Criteria = "1 workout",
-                    IsUnlocked = true
-                },
-                new AchievementShowcaseItem
-                {
-                    AchievementId = 2,
-                    Title = "10 Workouts",
-                    Description = "Complete 10 workouts",
-                    Criteria = "10 workouts",
-                    IsUnlocked = false
-                }
-            };
+            _clientService.GetAchievements(ValidClientId).Returns(_mockAchievements);
 
-            _achievementsRepository.GetAchievementShowcaseForClient(clientId).Returns(mockShowcaseItems);
+            _achievementsViewModel.LoadAchievementsCommand.Execute(ValidClientId);
 
-            // Act
-            _sut.LoadAchievementsCommand.Execute(clientId);
+            _achievementsViewModel.IsLoading.Should().BeFalse();
+            _achievementsViewModel.Achievements.Should().HaveCount(_mockAchievements.Count);
 
-            // Assert
-            _sut.IsLoading.Should().BeFalse(); // Returns to false in the finally block
-            _sut.Achievements.Should().HaveCount(2);
-
-            var firstAchievement = _sut.Achievements.First();
-            firstAchievement.AchievementId.Should().Be(1);
-            firstAchievement.Name.Should().Be("First Workout");
-            firstAchievement.Description.Should().Be("Complete your first workout");
-            firstAchievement.Criteria.Should().Be("1 workout");
+            var firstAchievement = _achievementsViewModel.Achievements.First();
+            firstAchievement.AchievementId.Should().Be(FirstAchievementId);
+            firstAchievement.Name.Should().Be(FirstAchievementName);
+            firstAchievement.Description.Should().Be(FirstAchievementDescription);
+            firstAchievement.Criteria.Should().Be(FirstAchievementCriteria);
             firstAchievement.IsUnlocked.Should().BeTrue();
-            firstAchievement.Icon.Should().Be("&#xE73E;");
+            firstAchievement.Icon.Should().Be(FirstAchievementIcon);
 
-            var secondAchievement = _sut.Achievements.Last();
-            secondAchievement.AchievementId.Should().Be(2);
-            secondAchievement.Name.Should().Be("10 Workouts");
-            secondAchievement.Description.Should().Be("Complete 10 workouts");
-            secondAchievement.Criteria.Should().Be("10 workouts");
+            var secondAchievement = _achievementsViewModel.Achievements.Last();
+            secondAchievement.AchievementId.Should().Be(SecondAchievementId);
+            secondAchievement.Name.Should().Be(SecondAchievementName);
+            secondAchievement.Description.Should().Be(SecondAchievementDescription);
+            secondAchievement.Criteria.Should().Be(SecondAchievementCriteria);
             secondAchievement.IsUnlocked.Should().BeFalse();
-            secondAchievement.Icon.Should().Be("&#xE72E;");
+            secondAchievement.Icon.Should().Be(SecondAchievementIcon);
         }
 
         [Fact]
         public void LoadAchievementsCommand_RepositoryThrowsException_ClearsAchievementsAndSetsLoadingToFalse()
         {
-            // Arrange
-            var clientId = 1;
-            _sut.Achievements.Add(new Achievement { AchievementId = 99 }); // Pre-populate to ensure it gets cleared
+            _achievementsViewModel.Achievements.Add(new Achievement { AchievementId = ExistingAchievementId });
 
-            _achievementsRepository.When(x => x.GetAchievementShowcaseForClient(clientId))
-                .Do(x => { throw new Exception("Database error"); });
+            _clientService.When(x => x.GetAchievements(ValidClientId))
+                .Do(x => { throw new Exception(DatabaseErrorMessage); });
 
-            // Act
-            var exception = Record.Exception(() => _sut.LoadAchievementsCommand.Execute(clientId));
+            var exception = Record.Exception(() => _achievementsViewModel.LoadAchievementsCommand.Execute(ValidClientId));
 
-            // Assert
             exception.Should().NotBeNull();
             exception.Should().BeOfType<Exception>();
-            _sut.Achievements.Should().BeEmpty(); // It clears before the exception is thrown
-            _sut.IsLoading.Should().BeFalse(); // Finally block should execute
+            _achievementsViewModel.Achievements.Should().BeEmpty();
+            _achievementsViewModel.IsLoading.Should().BeFalse();
         }
     }
 }
