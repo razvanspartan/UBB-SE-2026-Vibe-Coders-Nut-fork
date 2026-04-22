bfa3113c5bf42776e147eb333127ebf657033e7a
trueprodprog <trueproductm@gmail.com>
2026-04-22 22:56:12 +0300

    files to check

---
+++ VibeCoders.Tests/Unit/Services/CalendarExportServiceTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VibeCoders.Models;
+using VibeCoders.Services;
+using Xunit;
+
+namespace VibeCoders.Tests.Unit.Services
+{
+    public class CalendarExportServiceTests
+    {
+        private readonly CalendarExportService calendarExportService;
+
+        public CalendarExportServiceTests()
+        {
+            
+            this.calendarExportService = new CalendarExportService();
+        }
+
+ 
+
+        [Fact]
+        public void GenerateCalendar_Should_ThrowArgumentNullException_When_WorkoutTemplateIsNull()
+        {
+            
+            Action act = () => this.calendarExportService.GenerateCalendar(null!, 4, new[] { 1 });
+
+            
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(53)]
+        public void GenerateCalendar_Should_ThrowArgumentOutOfRangeException_When_DurationWeeksIsInvalid(int invalidWeeks)
+        {
+        
+            var workoutTemplate = new WorkoutTemplate { Name = "Test" };
+
+          
+            Action act = () => this.calendarExportService.GenerateCalendar(workoutTemplate, invalidWeeks, new[] { 1 });
+
+        
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void GenerateCalendar_Should_GenerateCorrectNumberOfEvents_ForMultipleWeeks()
+        {
+           
+            var workoutTemplate = new WorkoutTemplate { Id = 1, Name = "Full Body" };
+            int weeks = 3;
+            int[] days = { 1, 3, 5 }; 
+
+           
+            var result = this.calendarExportService.GenerateCalendar(workoutTemplate, weeks, days);
+
+            
+            int eventCount = this.CountStringOccurrences(result, "BEGIN:VEVENT");
+            eventCount.Should().Be(9);
+        }
+
+      
+        [Fact]
+        public async Task SaveCalendarToDownloadsAsync_Should_ReturnNull_When_ContentIsEmpty()
+        {
+            
+            var result = await this.calendarExportService.SaveCalendarToDownloadsAsync(string.Empty, "Workout");
+
+           
+            result.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("Upper Body!", "Upper-Body!")]   
+        [InlineData("Legs / Glutes", "Legs---Glutes")] 
+        [InlineData(null, "Workout")]                 
+        public async Task SaveCalendarToDownloadsAsync_Should_HandleInvalidCharactersInFileName(string? inputName, string expectedSubstring)
+        {
+           
+            var result = await this.calendarExportService.SaveCalendarToDownloadsAsync("test content", inputName);
+
+            result.Should().Contain(expectedSubstring);
+        }
+
+
+
+        private int CountStringOccurrences(string text, string pattern)
+        {
+            int count = 0;
+            int position = 0;
+            while ((position = text.IndexOf(pattern, position)) != -1)
+            {
+                position += pattern.Length;
+                count++;
+            }
+
+            return count;
+        }
+
+    }
+}
---
+++ VibeCoders.Tests/Unit/Services/TrainerService.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NSubstitute;
+using VibeCoders.Models;
+using VibeCoders.Repositories.Interfaces;
+using VibeCoders.Services;
+using Xunit;
+
+namespace VibeCoders.Tests.Unit.Services;
+
+public class TrainerServiceTests
+{
+    private readonly IRepositoryWorkoutTemplate mockWorkoutTemplateRepository;
+    private readonly IRepositoryWorkoutLog mockWorkoutLogRepository;
+    private readonly IRepositoryTrainer mockTrainerRepository;
+    private readonly IRepositoryNutrition mockNutritionRepository;
+    private readonly TrainerService trainerService;
+
+    public TrainerServiceTests()
+    {
+        this.mockWorkoutTemplateRepository = Substitute.For<IRepositoryWorkoutTemplate>();
+        this.mockWorkoutLogRepository = Substitute.For<IRepositoryWorkoutLog>();
+        this.mockTrainerRepository = Substitute.For<IRepositoryTrainer>();
+        this.mockNutritionRepository = Substitute.For<IRepositoryNutrition>();
+
+        this.trainerService = new TrainerService(
+            this.mockWorkoutTemplateRepository,
+            this.mockWorkoutLogRepository,
+            this.mockTrainerRepository,
+            this.mockNutritionRepository);
+    }
+
+    [Fact]
+    public void SaveWorkoutFeedback_Should_ReturnFalse_When_LogIsNull()
+    {
+        bool result = this.trainerService.SaveWorkoutFeedback(null!);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void SaveWorkoutFeedback_Should_CallRepository_When_LogIsValid()
+    {
+        var log = new WorkoutLog { Id = 10, Rating = 5, TrainerNotes = "Good job" };
+        this.mockWorkoutLogRepository.UpdateWorkoutLogFeedback(10, 5, "Good job").Returns(true);
+
+        bool result = this.trainerService.SaveWorkoutFeedback(log);
+
+        result.Should().BeTrue();
+        this.mockWorkoutLogRepository.Received(1).UpdateWorkoutLogFeedback(10, 5, "Good job");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void AssignNewRoutine_Should_ReturnError_When_RoutineNameIsInvalid(string? invalidName)
+    {
+        var exercises = new List<TemplateExercise> { new TemplateExercise { Name = "Pushup" } };
+
+        var result = this.trainerService.AssignNewRoutine(null, 1, invalidName!, exercises);
+
+        result.Success.Should().BeFalse();
+        result.ErrorMessage.Should().Be("Routine Name cannot be empty.");
+    }
+
+    [Fact]
+    public void AssignNewRoutine_Should_ReturnError_When_ExercisesAreEmpty()
+    {
+        var result = this.trainerService.AssignNewRoutine(null, 1, "My Routine", Enumerable.Empty<TemplateExercise>());
+
+        result.Success.Should().BeFalse();
+        result.ErrorMessage.Should().Be("You must add at least one exercise to the routine.");
+    }
+
+    [Fact]
+    public void AssignNewRoutine_Should_ReturnSuccess_And_SaveToRepository_When_Valid()
+    {
+        var exercises = new List<TemplateExercise> { new TemplateExercise { Name = "Squat" } };
+        this.mockTrainerRepository.SaveTrainerWorkout(Arg.Any<WorkoutTemplate>()).Returns(true);
+
+        var result = this.trainerService.AssignNewRoutine(null, 1, "Leg Day", exercises);
+
+        result.Success.Should().BeTrue();
+        this.mockTrainerRepository.Received(1).SaveTrainerWorkout(Arg.Is<WorkoutTemplate>(t => t.Name == "Leg Day"));
+    }
+
+    [Fact]
+    public void AssignNutritionPlan_Should_ReturnFalse_When_ClientIdIsInvalid()
+    {
+        var plan = new NutritionPlan();
+        bool result = this.trainerService.AssignNutritionPlan(plan, 0);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void AssignNutritionPlan_Should_CallRepository_When_DataIsValid()
+    {
+        var plan = new NutritionPlan();
+        int validClientId = 5;
+
+        bool result = this.trainerService.AssignNutritionPlan(plan, validClientId);
+
+        result.Should().BeTrue();
+        this.mockNutritionRepository.Received(1).SaveNutritionPlanForClient(plan, validClientId);
+    }
+
+    [Fact]
+    public void CreateAndAssignNutritionPlan_Should_SetDatesToMidnight()
+    {
+        var start = new DateTime(2026, 1, 1, 14, 30, 0);
+        var end = new DateTime(2026, 1, 7, 14, 30, 0);
+        int clientId = 1;
+
+        this.trainerService.CreateAndAssignNutritionPlan(start, end, clientId);
+
+        this.mockNutritionRepository.Received(1).SaveNutritionPlanForClient(
+            Arg.Is<NutritionPlan>(p => p.StartDate == start.Date && p.EndDate == end.Date),
+            clientId);
+    }
+}
