7cb6cb4c12df35d8900011dd8a59ed00115478c0
trueprodprog <trueproductm@gmail.com>
2026-04-23 11:29:35 +0300

    NUT_429_430 (#50)
    * WorkoutHistoryItem
    
    * WorkOutLogsViewModelTests
    
    * abbreviations were removed
    
    * better coverage

---
+++ VibeCoders.Tests/Unit/ViewModels/WorkoutHistoryItemViewModelTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+using NSubstitute;
+using VibeCoders.Models.Analytics;
+using VibeCoders.Repositories.Interfaces;
+using VibeCoders.ViewModels;
+using Windows.UI;
+using Xunit;
+
+namespace VibeCoders.Tests.Unit.ViewModels;
+
+public class WorkoutHistoryItemViewModelTests
+{
+    private readonly IWorkoutAnalyticsStore mockWorkoutAnalyticsStore;
+    private readonly long clientIdentifier = 123;
+
+    public WorkoutHistoryItemViewModelTests()
+    {
+        this.mockWorkoutAnalyticsStore = Substitute.For<IWorkoutAnalyticsStore>();
+    }
+
+    private WorkoutHistoryRow CreateWorkoutHistoryRow(string workoutName = "Full Body", string intensityTag = "Moderate") => new WorkoutHistoryRow
+    {
+        Id = 1,
+        WorkoutName = workoutName,
+        LogDate = new DateTime(2026, 4, 23),
+        DurationSeconds = 3600,
+        TotalCaloriesBurned = 500,
+        IntensityTag = intensityTag
+    };
+
+    [Fact]
+    public void Constructor_Should_HandleEmptyWorkoutName_WithFallback()
+    {
+        var workoutHistoryRow = this.CreateWorkoutHistoryRow(workoutName: "");
+
+        var workoutHistoryItemViewModel = new WorkoutHistoryItemViewModel(this.mockWorkoutAnalyticsStore, this.clientIdentifier, workoutHistoryRow);
+
+        workoutHistoryItemViewModel.Title.Should().Be("Workout");
+    }
+
+    [Theory]
+    [InlineData("Light")]
+    [InlineData("Moderate")]
+    [InlineData("Intense")]
+    [InlineData("Unknown")]
+    public void IntensityTag_Should_MatchRowData(string expectedIntensityTag)
+    {
+        var workoutHistoryRow = this.CreateWorkoutHistoryRow(intensityTag: expectedIntensityTag);
+
+        var workoutHistoryItemViewModel = new WorkoutHistoryItemViewModel(this.mockWorkoutAnalyticsStore, this.clientIdentifier, workoutHistoryRow);
+
+        workoutHistoryItemViewModel.IntensityTag.Should().Be(expectedIntensityTag);
+    }
+
+    [Fact]
+    public async Task OnIsExpandedChanged_Should_OnlyLoadDataOnce_WhenExpandedMultipleTimes()
+    {
+        var workoutHistoryRow = this.CreateWorkoutHistoryRow();
+        var workoutHistoryItemViewModel = new WorkoutHistoryItemViewModel(this.mockWorkoutAnalyticsStore, this.clientIdentifier, workoutHistoryRow);
+
+        var emptyWorkoutSessionDetail = new WorkoutSessionDetail
+        {
+            Sets = Array.Empty<WorkoutSetRow>(),
+            ExerciseCalories = Array.Empty<ExerciseCalorieInfo>()
+        };
+
+        this.mockWorkoutAnalyticsStore.GetWorkoutSessionDetailAsync(this.clientIdentifier, workoutHistoryItemViewModel.WorkoutLogId, Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult<WorkoutSessionDetail?>(emptyWorkoutSessionDetail));
+
+        workoutHistoryItemViewModel.IsExpanded = true;
+        workoutHistoryItemViewModel.IsExpanded = false;
+        workoutHistoryItemViewModel.IsExpanded = true;
+
+        await Task.Delay(100);
+
+        await this.mockWorkoutAnalyticsStore.Received(1).GetWorkoutSessionDetailAsync(this.clientIdentifier, workoutHistoryItemViewModel.WorkoutLogId, Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task LoadDetailAsync_Should_GroupSetsByExerciseName_And_MaintainOrder()
+    {
+        var workoutHistoryRow = this.CreateWorkoutHistoryRow();
+        var workoutHistoryItemViewModel = new WorkoutHistoryItemViewModel(this.mockWorkoutAnalyticsStore, this.clientIdentifier, workoutHistoryRow);
+
+        var workoutSessionDetail = new WorkoutSessionDetail
+        {
+            Sets = new List<WorkoutSetRow>
+            {
+                new WorkoutSetRow { ExerciseName = "Squat", SetIndex = 0, ActualReps = 10 },
+                new WorkoutSetRow { ExerciseName = "Squat", SetIndex = 1, ActualReps = 12 },
+                new WorkoutSetRow { ExerciseName = "Bench Press", SetIndex = 0, ActualReps = 8 }
+            },
+            ExerciseCalories = new List<ExerciseCalorieInfo>
+            {
+                new ExerciseCalorieInfo { ExerciseName = "Squat", CaloriesBurned = 45 }
+            }
+        };
+
+        this.mockWorkoutAnalyticsStore.GetWorkoutSessionDetailAsync(this.clientIdentifier, workoutHistoryItemViewModel.WorkoutLogId, Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult<WorkoutSessionDetail?>(workoutSessionDetail));
+
+        workoutHistoryItemViewModel.IsExpanded = true;
+        await Task.Delay(100);
+
+        workoutHistoryItemViewModel.ExerciseSetGroups.Should().HaveCount(2);
+
+        var exerciseGroup = workoutHistoryItemViewModel.ExerciseSetGroups.First(group => group.ExerciseName == "Squat");
+        exerciseGroup.Sets.Should().HaveCount(2);
+
+        exerciseGroup.Sets[0].RepsDisplay.Should().Be("10");
+
+        workoutHistoryItemViewModel.ExerciseCalories.Should().HaveCount(1);
+        workoutHistoryItemViewModel.ExerciseCalories[0].CaloriesBurned.Should().Be(45);
+    }
+
+    [Fact]
+    public async Task LoadDetailAsync_Should_ClearCollections_When_DetailIsNull()
+    {
+        var workoutHistoryRow = this.CreateWorkoutHistoryRow();
+        var workoutHistoryItemViewModel = new WorkoutHistoryItemViewModel(this.mockWorkoutAnalyticsStore, this.clientIdentifier, workoutHistoryRow);
+        this.mockWorkoutAnalyticsStore.GetWorkoutSessionDetailAsync(this.clientIdentifier, workoutHistoryItemViewModel.WorkoutLogId, Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult<WorkoutSessionDetail?>(null));
+
+        workoutHistoryItemViewModel.IsExpanded = true;
+        await Task.Delay(100);
+
+        workoutHistoryItemViewModel.ExerciseSetGroups.Should().BeEmpty();
+        workoutHistoryItemViewModel.IsLoadingDetail.Should().BeFalse(); 
+    }
+
+}
---
+++ VibeCoders.Tests/Unit/ViewModels/WorkoutLogsViewModelTests.cs
@@ -0,0 +1,163 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VibeCoders.Models;
+using VibeCoders.Services;
+using VibeCoders.Services.Interfaces;
+using VibeCoders.ViewModels;
+using Xunit;
+
+namespace VibeCoders.Tests.Unit.ViewModels;
+
+public class WorkoutLogsViewModelTests
+{
+    private readonly INavigationService mockNavigationService;
+    private readonly IClientService mockClientService;
+    private readonly WorkoutLogsViewModel workoutLogsViewModel;
+
+    public WorkoutLogsViewModelTests()
+    {
+        this.mockNavigationService = Substitute.For<INavigationService>();
+        this.mockClientService = Substitute.For<IClientService>();
+        this.workoutLogsViewModel = new WorkoutLogsViewModel(this.mockNavigationService, this.mockClientService);
+    }
+
+    [Fact]
+    public void LoadLogs_Should_PopulateLogs_When_ServiceReturnsData()
+    {
+        int clientIdentifier = 1;
+        var sampleWorkoutLogs = new List<WorkoutLog>
+        {
+            new WorkoutLog { Id = 10, WorkoutName = "Morning Run", Date = DateTime.Now, Exercises = new() }
+        };
+        this.mockClientService.GetWorkoutHistoryForClient(clientIdentifier).Returns(sampleWorkoutLogs);
+
+        this.workoutLogsViewModel.LoadLogsCommand.Execute(clientIdentifier);
+
+        this.workoutLogsViewModel.Logs.Should().HaveCount(1);
+        this.workoutLogsViewModel.Logs[0].WorkoutName.Should().Be("Morning Run");
+        this.workoutLogsViewModel.ShowEmptyState.Should().BeFalse();
+        this.workoutLogsViewModel.IsLoading.Should().BeFalse();
+    }
+
+    [Fact]
+    public void LoadLogs_Should_HandleError_When_ServiceThrows()
+    {
+        this.mockClientService.GetWorkoutHistoryForClient(Arg.Any<int>())
+            .Returns(_ => throw new Exception("Database Timeout"));
+
+        this.workoutLogsViewModel.LoadLogsCommand.Execute(1);
+
+        this.workoutLogsViewModel.ErrorMessage.Should().Contain("Database Timeout");
+        this.workoutLogsViewModel.ShowEmptyState.Should().BeTrue();
+        this.workoutLogsViewModel.Logs.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ToggleEditMode_Should_SwitchStateCorrectly()
+    {
+        var workoutLog = new WorkoutLog { Id = 1, Exercises = new() };
+        var workoutLogItemViewModel = new WorkoutLogItemViewModel(workoutLog, this.mockClientService);
+
+        this.workoutLogsViewModel.ToggleEditModeCommand.Execute(workoutLogItemViewModel);
+        bool isFirstEditState = workoutLogItemViewModel.IsEditMode;
+
+        this.workoutLogsViewModel.ToggleEditModeCommand.Execute(workoutLogItemViewModel);
+        bool isSecondEditState = workoutLogItemViewModel.IsEditMode;
+
+        isFirstEditState.Should().BeTrue();
+        isSecondEditState.Should().BeFalse();
+    }
+
+    [Fact]
+    public void CancelEditMode_Should_RevertChanges_ToOriginalLogData()
+    {
+        var workoutLog = new WorkoutLog
+        {
+            Id = 1,
+            Exercises = new List<LoggedExercise>
+            {
+                new LoggedExercise
+                {
+                    ExerciseName = "Pushups",
+                    Sets = new List<LoggedSet> { new LoggedSet { ActualReps = 10 } }
+                }
+            }
+        };
+        var workoutLogItemViewModel = new WorkoutLogItemViewModel(workoutLog, this.mockClientService);
+
+        workoutLogItemViewModel.EnterEditMode();
+        workoutLogItemViewModel.Exercises[0].Sets[0].Reps = 20;
+
+        workoutLogItemViewModel.CancelEditMode();
+
+        workoutLogItemViewModel.Exercises[0].Sets[0].Reps.Should().Be(10);
+        workoutLogItemViewModel.IsEditMode.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(15.0, 15)]
+    [InlineData(15.6, 16)]
+    [InlineData(double.NaN, null)]
+    public void WorkoutLogSetEditor_RepsInput_Should_MapCorrectIntValues(double userInput, int? expectedRepetitions)
+    {
+        var workoutLogSetEditorViewModel = new WorkoutLogSetEditorViewModel { SetNumber = 1 };
+
+        workoutLogSetEditorViewModel.RepsInput = userInput;
+
+        workoutLogSetEditorViewModel.Reps.Should().Be(expectedRepetitions);
+    }
+
+    [Fact]
+    public void ToLoggedExercise_Should_MapViewModelData_BackToModel()
+    {
+        var loggedExercise = new LoggedExercise { ExerciseName = "Squat", Sets = new() };
+        var workoutLogExerciseSummary = new WorkoutLogExerciseSummary(loggedExercise);
+        workoutLogExerciseSummary.Sets.Add(new WorkoutLogSetEditorViewModel { Reps = 10, Weight = 100.5 });
+
+        var mappedLoggedExercise = workoutLogExerciseSummary.ToLoggedExercise(99);
+
+        mappedLoggedExercise.WorkoutLogId.Should().Be(99);
+        mappedLoggedExercise.ExerciseName.Should().Be("Squat");
+        mappedLoggedExercise.Sets.Should().HaveCount(1);
+        mappedLoggedExercise.Sets[0].ActualReps.Should().Be(10);
+        mappedLoggedExercise.Sets[0].ActualWeight.Should().Be(100.5);
+    }
+
+    [Fact]
+    public void SaveEditedLog_Should_DoNothing_When_ItemIsNotInEditMode()
+    {
+        var workoutLog = new WorkoutLog { Id = 1, Exercises = new() };
+        var workoutLogItemViewModel = new WorkoutLogItemViewModel(workoutLog, this.mockClientService);
+        this.workoutLogsViewModel.SaveEditedLogCommand.Execute(workoutLogItemViewModel);
+        this.mockClientService.DidNotReceive().UpdateWorkoutLog(Arg.Any<WorkoutLog>());
+    }
+
+    [Fact]
+    public void StartWorkout_Should_NavigateToActiveWorkout()
+    {
+        this.workoutLogsViewModel.StartWorkoutCommand.Execute(99);
+
+        this.mockNavigationService.Received(1).NavigateToActiveWorkout(99);
+    }
+
+    [Theory]
+    [InlineData("Feeling weak", "Feeling weak")]
+    [InlineData(null, "Performance: 80% of target reps achieved.")]
+    public void ExerciseSummary_Tooltip_Should_HandleAdjustmentNotes(string? adjustmentNote, string expectedTooltip)
+    {
+        var loggedExercise = new LoggedExercise
+        {
+            ExerciseName = "Pushups",
+            AdjustmentNote = adjustmentNote ?? string.Empty,
+            PerformanceRatio = 0.8f,
+            Sets = new()
+        };
+
+        var workoutLogExerciseSummary = new WorkoutLogExerciseSummary(loggedExercise);
+
+        workoutLogExerciseSummary.TooltipText.Should().Be(expectedTooltip);
+    }
+}
